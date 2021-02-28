6c9396c67ca673919a855c6f7e275fcc71742986
VitorGouveia <vitor.gouevia10@gmail.com>
2021-02-28 18:44:20 -0300

    app interface done

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,63 @@
-if("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
+const $ = document.querySelector.bind(document)
+
+const html = {
+  items: $("#cart"),
+  cardInfo: $("#cardInfo")
+}
+
+let Counter = 0
+
+function Display(element, state, display) {
+  if(state == "show") {
+    element.style.display = display
+  } else if(state == "hide") {
+    element.style.display = "none"
+  }
+}
+
+$("#front").addEventListener("click", () => {
+  Counter += 1
+
+  if(Counter == 1) {
+    Display(html.items, "hide")
+    Display(html.cardInfo, "show", "flex")
+    Display($("#back"), "show", "flex")
+    $("#front").innerHTML = "Finalizar a compra"
+  } else if(Counter = 2) {
+    $("#cardInfo form").submit()
+    // alert("Compra finalizada")
+    // window.location.href = "http://localhost:5500/index.html"
+  }
+})
+
+$("#back").addEventListener("click", () => {
+  Counter -= 1
+
+  if(Counter == 0) {
+    Display($("#back"), "hide")
+    Display(html.cardInfo, "hide")
+    Display(html.items, "show", "flex")
+    $("#front").innerHTML = "Próximo &rarr;"
+    $("#front").type = "button"
+  }
+})
+
+console.log($("#cardInfo select").value)
+$("select").addEventListener("change", () => {
+  if($("select").value == 1) {
+    $("#cardInfo form").style.display = "none"
+  } else if($("select").value == 2 || $("select").value == 3) {
+    $("#cardInfo form").style.display = "block"
+  }
+})
+
+if(Counter == 0) {
+  Display($("#back"), "hide")
+  Display(html.cardInfo, "hide")
+  Display(html.items, "show", "flex")
+} else if(Counter == 1) {
+  Display(html.items, "hide")
+  Display(html.cardInfo, "show", "flex")
+  Display($("#back"), "show", "flex")
+  $("#front").innerHTML = "Finalizar a compra"
 }
